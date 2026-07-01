e7fbd1121423c654966f894be129ca847316a0ab
Divyansh Kaushik <kaushikdivyansh39@gmail.com>
2026-07-01 23:53:01 +0530

    Working on train_model.py

---
+++ train_model.py
@@ -0,0 +1,16 @@
+import os
+import pandas as pd
+import numpy as np
+import pickle
+
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import {
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score
+}
+
+data = pd.read_csv("dataset/Students Performance Dataset.csv")
+
+
