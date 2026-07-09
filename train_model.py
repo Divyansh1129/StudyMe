b4e822e1966ab691676414a97c3f9d269d1dadf5
Divyansh Kaushik <kaushikdivyansh39@gmail.com>
2026-07-09 19:40:32 +0530

    fixed the model train

--- train_model.py
+++ train_model.py
@@ -4,7 +4,7 @@ import numpy as np
 import pickle
 
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split 
 from sklearn.metrics import (
     mean_absolute_error,
     mean_squared_error,
